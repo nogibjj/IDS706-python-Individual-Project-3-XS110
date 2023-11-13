620fe242fee669df47bd6c2b21779af3412f746a
Xinyi Sheng <xs110@duke.edu>
2023-11-13 14:35:58 -0500

    action

--- trigger.py
+++ trigger.py
@@ -12,21 +12,19 @@ server_hostname = os.getenv("DATABRICKS_HOST")
 job_id = os.getenv("JOB_ID")
 access_token = os.getenv("DATABRICKS_TOKEN")
 url = f'https://{server_hostname}/api/2.0/jobs/run-now'
-print(server_hostname)
-print(job_id)
-print(access_token)
-# headers = {
-#     'Authorization': f'Bearer {access_token}',
-#     'Content-Type': 'application/json',
-# }
 
-# data = {
-#     'job_id': job_id
-# }
+headers = {
+    'Authorization': f'Bearer {access_token}',
+    'Content-Type': 'application/json',
+}
 
-# response = requests.post(url, headers=headers, json=data)
+data = {
+    'job_id': job_id
+}
 
-# if response.status_code == 200:
-#     print('Job run successfully triggered')
-# else:
-#     print(f'Error: {response.status_code}, {response.text}')
+response = requests.post(url, headers=headers, json=data)
+
+if response.status_code == 200:
+    print('Job run successfully triggered')
+else:
+    print(f'Error: {response.status_code}, {response.text}')
